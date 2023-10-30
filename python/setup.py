0e6d78b5d9b5610e70dbd996f0ad031002f622f1
JSpiner <jspiner@naver.com>
2023-10-30 19:49:15 +0900

    bump version 0.0.2

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="teslasdk",
-    version="0.0.1",
+    version="0.0.2",
     author="JSpiner",
     author_email="jspiner@naver.com",
     description="TeslaSdk",
